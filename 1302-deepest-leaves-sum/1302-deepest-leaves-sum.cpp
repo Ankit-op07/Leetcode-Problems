35f1d4c8d91b89e1f2ef1c4a0ae588fd576a1bd5
Ankit-op07 <85008674+Ankit-op07@users.noreply.github.com>
2022-01-14 01:21:41 +0530

    Time: 139 ms (27.98%), Space: 59.7 MB (95.33%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -16,34 +16,47 @@ public:
  
     
     
- //using bfs
-    int deepestLeavesSum(TreeNode* root) {
-//queue
-        // so basically we will find sum of 
-        int level_sum=0;
-        queue<TreeNode*>q;
-        q.push(root);
+//  //using bfs
+//     int deepestLeavesSum(TreeNode* root) {
+// //queue
+//         // so basically we will find sum of 
+//         int level_sum=0;
+//         //we pushed the root in queue
+//         queue<TreeNode*>q;
+//         q.push(root);
         
-        while(!q.empty()){
-            level_sum=0; // initialize everytime with 0
-            int size = q.size(); //this is the size of the queue
-            for(int i=0;i<size;i++){
+//         while(!q.empty()){
+//             level_sum=0; // initialize everytime with 0
+//             int size = q.size(); //this is the size of the queue
+//             //this loop is for traversing the level
+            
+//             for(int i=0;i<size;i++){
                 
-                TreeNode *n = q.front(); 
-                level_sum += n->val;
-                q.pop();
-                if(n->left){
-                    q.push(n->left);
-                }
-                if(n->right){
-                    q.push(n->right);
-                }
+//                 TreeNode *n = q.front(); 
+//                 level_sum += n->val;
+//                 q.pop();
+//                 if(n->left){
+//                     q.push(n->left);
+//                 }
+//                 if(n->right){
+//                     q.push(n->right);
+//                 }
                 
                 
-            }
-        }
-        return level_sum;
+//             }
+//         }
+//         return level_sum;
         
          
-    }
+//     }
+    int deepest = 0, sum = 0;
+int deepestLeavesSum(TreeNode* root, int depth = 0) {
+	if(!root) return 0;  // current node is NULL - just return.
+	if(!root -> left && !root -> right) // if current is a leaf node
+		if(depth == deepest) sum += root -> val;  // if depth is exactly equal to deepest, add to sum
+		else if(depth > deepest) sum = root -> val, deepest = depth; // found a more deeper leaf node, reset sum and update deepest
+	deepestLeavesSum(root -> left, depth + 1);   // recurse for the left, 
+	deepestLeavesSum(root -> right, depth + 1);  // and the right node of the current node
+	return sum; // finally return the sum accumulated till now which will be sum of value of deepest leaves
+}
 };

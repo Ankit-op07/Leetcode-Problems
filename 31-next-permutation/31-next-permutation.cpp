4c910807708c76c8ea8c43f5fdf430219d81371a
Ankit-op07 <85008674+Ankit-op07@users.noreply.github.com>
2022-02-28 13:00:36 +0530

    Time: 0 ms (100.00%), Space: 12.1 MB (75.39%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-     
+     next_permutation(nums.begin(),nums.end());
         
-        // At first i will find an element wich is less than to it's next element
-        int n= nums.size();
         
-        int ind=-1;
         
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                ind =i;
-                break;
-            }
-        }
+//           int n= nums.size();
+//         // At first i will find an element wich is less than to it's next element
+      
         
-        // then again i will traverse from right to left and find a number which is slighly greater than the number on index ind.
+//         int ind=-1; // ind will keep track of the index of that number
         
-        if(ind!=-1){
-        for(int i=n-1;i>=0;i--){
-            if(nums[i]>nums[ind]){
-                int temp = nums[i];
-                nums[i]=nums[ind];
-                nums[ind]=temp;
-                break;
-            }
-        }
-        }
+//         for(int i=n-2;i>=0;i--){
+//             if(nums[i]<nums[i+1]){
+//                 ind =i;
+//                 break;
+//             }
+//         }
         
-        // now i will reverse the subarray which is in the right of the index ind.
-        reverse(nums.begin()+ind+1,nums.end());
+//         // then again i will traverse from right to left and find a number which is slighly greater than the number on index ind.
+        
+//         if(ind!=-1){
+//         for(int i=n-1;i>=0;i--){
+//             if(nums[i]>nums[ind]){
+//                 int temp = nums[i];
+//                 nums[i]=nums[ind];
+//                 nums[ind]=temp;
+//                 break;
+//             }
+//         }
+//         }
+        
+//         // now i will reverse the subarray which is in the right of the index ind.
+//         reverse(nums.begin()+ind+1,nums.end());
         
     }
 };

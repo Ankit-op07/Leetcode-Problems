0f8879c448bcbc5f4af9b2852626b1d2f974f0ef
Ankit-op07 <85008674+Ankit-op07@users.noreply.github.com>
2022-03-02 02:13:03 +0530

    Time: 17 ms (94.63%), Space: 11.7 MB (42.79%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
+      stack<int>st;
         
-        stack<int>st; // helper stack for finding matching parentheses
-        
-        for(int i=0;i<s.length();++i){
-            if(s[i]=='('){ // for open parentheses push into stack
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
                 st.push(i);
-            }
-            else if(s[i]==')'){ // for closing parentheses
-                // if no matching previous open parentheses found, we need to remove the index of that open parentheses from "s" so for now we are marking it with special character '#'
-                if(st.empty()){ 
-                    s[i]='#';
-                }
-                else{
-                    // if matching open parentheses found remove that from the stack
+            }else if(s[i]==')'){
+                
+                if(st.empty()==true){
+                 s[i]='*';   
+                }else{
                     st.pop();
                 }
             }
         }
-
-        // if stack is not empty, that means it contains open parentheses indexes which don't have any matching closing parentheses
+        
         while(!st.empty()){
-            s[st.top()]='#';
+            s[st.top()]='*';
             st.pop();
         }
         
         string ans="";
-        for(int i=0;i<s.length();++i){
-            if(s[i]!='#'){ // append not marked character to the end of "ans"
-                ans.push_back(s[i]);
+        for(int i=0;i<s.size();i++){
+            if(s[i]!='*'){
+                ans+=s[i];
             }
         }
-        
         return ans;
     }
 };

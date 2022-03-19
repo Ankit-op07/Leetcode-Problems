115b7ced7ffffec3f63d9f88fe37195ffa8e3dba
Ankit-op07 <85008674+Ankit-op07@users.noreply.github.com>
2022-03-20 00:49:11 +0530

    Time: 12 ms (95.39%), Space: 21.7 MB (64.40%) - LeetHub

--- 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
@@ -15,25 +15,9 @@ public:
         
         
 //     }
-    TreeNode* removeLeafNodes(TreeNode* root, int target) {
-        // dfs(root,-1,target);
-        if(root->left==NULL && root->right==NULL && root->val==target){
-            return NULL;
-        }
-        
-        if(root->left){
-            root->left=removeLeafNodes(root->left,target);
-           
-        }
-        if(root->right){
-            root->right=removeLeafNodes(root->right,target);
-        }
-        
-         if(root->left==NULL && root->right==NULL && root->val==target){
-            return NULL;
-        }
-        
-        
-        return root;
+     TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if (root->left) root->left = removeLeafNodes(root->left, target);
+        if (root->right) root->right = removeLeafNodes(root->right, target);
+        return root->left == root->right && root->val == target ? nullptr : root;
     }
 };

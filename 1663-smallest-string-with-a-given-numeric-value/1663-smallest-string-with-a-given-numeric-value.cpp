453358130e21b93ab9409a316e08b3f77343c0de
Ankit-op07 <85008674+Ankit-op07@users.noreply.github.com>
2022-03-22 10:47:29 +0530

    Time: 40 ms (85.12%), Space: 21 MB (99.12%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
+//     string getSmallestString(int n, int k) {
+//         int total=n*26;
+        
+// string alphabet="aabcdefghijklmnopqrstuvwxyz";
+        
+//         string ans;
+//         for(int i=0;i<n;i++){
+//             int diff=total-k;
+//             if(diff>=26){
+//                 ans.push_back(alphabet[1]);
+//                 total-=26;
+//                 k-=1;
+//             }else{
+//                 int d=total-k;
+//                 int ind=26-d;
+//                 ans.push_back(alphabet[ind]);
+//                 k-=ind;
+//                 total-=26;
+//             }
+//         }
+//         return ans;
+//     }
     string getSmallestString(int n, int k) {
-        int total=n*26;
-        
-string alphabet="aabcdefghijklmnopqrstuvwxyz";
-        
-        string ans;
-        for(int i=0;i<n;i++){
-            int diff=total-k;
-            if(diff>=26){
-                ans.push_back(alphabet[1]);
-                total-=26;
-                k-=1;
-            }else{
-                int d=total-k;
-                int ind=26-d;
-                ans.push_back(alphabet[ind]);
-                k-=ind;
-                total-=26;
-            }
-        }
-        return ans;
+    string res = string(n, 'a');
+    k -= n;
+    while (k > 0) {
+        res[--n] += min(25, k);
+        k -= min(25, k);
     }
+    return res;
+}
 };

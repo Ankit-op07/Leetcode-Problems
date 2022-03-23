109c76879e6a732e20cf1191b14d921c417b53fe
Ankit-op07 <85008674+Ankit-op07@users.noreply.github.com>
2022-03-24 02:43:24 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (99.60%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -15,21 +15,34 @@ public:
     void getCombination(int ind,vector<int>&comb,vector<int>&temp, vector<vector<int>>&ans,int n,int k){
         
         
-        if(ind==comb.size()){
-            if(!n && temp.size()==k){
-                ans.push_back(temp);
-            }
+//         if(ind==comb.size()){
+//             if(!n && temp.size()==k){
+//                 ans.push_back(temp);
+//             }
+//             return;
+//         }
+        
+//         if(comb[ind]<=n){
+//             temp.push_back(comb[ind]);
+//             getCombination(ind+1,comb,temp,ans,n-comb[ind],k);
+//             temp.pop_back();
+//         }
+        
+//         getCombination(ind+1,comb,temp,ans,n,k);
+        
+        if(!n && temp.size()==k){
+            ans.push_back(temp);
             return;
         }
         
-        if(comb[ind]<=n){
-            temp.push_back(comb[ind]);
-            getCombination(ind+1,comb,temp,ans,n-comb[ind],k);
-            temp.pop_back();
+        for(int i=ind;i<comb.size();i++){
+            
+            if(comb[i]<=n){
+                temp.push_back(comb[i]);
+                getCombination(i+1,comb,temp,ans,n-comb[i],k);
+                temp.pop_back();
+            }
         }
-        
-        getCombination(ind+1,comb,temp,ans,n,k);
-        
         
     }
 };

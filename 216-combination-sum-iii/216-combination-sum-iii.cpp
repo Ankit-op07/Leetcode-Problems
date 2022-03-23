628977c45f14a32155404d09006fd88dac5d728a
Ankit-op07 <85008674+Ankit-op07@users.noreply.github.com>
2022-03-24 02:38:56 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (81.12%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+   
+    
+    vector<vector<int>> combinationSum3(int k, int n) {
+      vector<int>comb={1,2,3,4,5,6,7,8,9};
+        vector<vector<int>>ans;
+        
+        int index=0;
+        vector<int>temp;
+        getCombination(index,comb,temp,ans,n,k);
+        return ans;
+    }
+    
+    void getCombination(int ind,vector<int>&comb,vector<int>&temp, vector<vector<int>>&ans,int n,int k){
+        
+        
+        if(ind==comb.size()){
+            if(!n && temp.size()==k){
+                ans.push_back(temp);
+            }
+            return;
+        }
+        
+        if(comb[ind]<=n){
+            temp.push_back(comb[ind]);
+            getCombination(ind+1,comb,temp,ans,n-comb[ind],k);
+            temp.pop_back();
+        }
+        
+        getCombination(ind+1,comb,temp,ans,n,k);
+        
+        
+    }
+};

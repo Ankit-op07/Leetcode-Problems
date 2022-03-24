8d1a257a81f65802a543d2940fd5455868dce316
Ankit-op07 <85008674+Ankit-op07@users.noreply.github.com>
2022-03-25 01:22:42 +0530

    Time: 2 ms (32.13%), Space: 6.2 MB (15.42%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int dp[46]={0};
-    int climbStairs(int n) {
-        
-        if(n==1 || n==2){
-            return dp[n]=n;
-        }
-        
-        if(dp[n]!=0) return dp[n];
-        
-        return dp[n]=climbStairs(n-1)+climbStairs(n-2);
 
+    int climbStairs(int n) {
+        if(n==1) return 1;
+vector<int>dp(n+1,-1);
+        dp[1]=1;
+        dp[2]=2;
+        
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };

34de58a3c277dc95b8ba6bfb0c5dcd2c2d568618
Ankit-op07 <85008674+Ankit-op07@users.noreply.github.com>
2022-03-26 19:28:33 +0530

    Time: 109 ms (77.04%), Space: 9.1 MB (61.19%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -2,17 +2,34 @@ class Solution {
 public:
     
     // 1.Top-Down Recursion
+    // 2.Memoization of Recursion
+    // 3.Tabulation 
+    // 4.Space optimization (if possible)
     
-    int dp[10000]={0};
+    
     int numSquares(int n) {
-        if(n==0 || n==1) return dp[n]=n;
-        if(dp[n]!=0) return dp[n];
-      int ans=INT_MAX;
-        for(int i=1;i*i<=n;i++){
-            int num=n-i*i;
-            ans=min(ans,numSquares(num)+1);
+        vector<int>dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=1;
+        
+        for(int i=2;i<=n;i++){
+            int ans=INT_MAX;
+            for(int j=1;j*j<=i;j++){
+                int num=i-j*j;
+                ans=min(ans,dp[num]+1);
+            }
+            dp[i]=ans;
         }
-        return dp[n]=ans;
+        return dp[n];
+        
+      //   if(n==0 || n==1) return dp[n]=n;
+      //   if(dp[n]!=0) return dp[n];
+      // int ans=INT_MAX;
+      //   for(int i=1;i*i<=n;i++){
+      //       int num=n-i*i;
+      //       ans=min(ans,numSquares(num)+1);
+      //   }
+      //   return dp[n]=ans;
     }
     
 };

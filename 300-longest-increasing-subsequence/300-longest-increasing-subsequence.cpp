6b610fde75f533552a48acb9caaa1f878e8adb69
Ankit-op07 <85008674+Ankit-op07@users.noreply.github.com>
2022-03-26 18:53:34 +0530

    Time: 248 ms (61.56%), Space: 10.6 MB (14.98%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,37 +1,58 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-       int n=nums.size();
         
-        int ans=INT_MIN;
-        
+        int n=nums.size();
         vector<int>dp(n,-1);
+        dp[0]=1;
         
-        for(int i=0;i<n;i++){
-            if(dp[i]!=-1){
-                ans=max(ans,dp[i]);
-            }else{
-                ans=max(ans,  helper(i,nums,dp));
+        for(int i=1;i<n;i++){
+            int ans=1;
+            for(int j=0;j<i;j++){
+                 if(nums[i]>nums[j]){
+                ans=max(ans,dp[j]+1);
             }
-            
+            }
+            dp[i]=ans;
         }
+      return *max_element(dp.begin(),dp.end());
+    }
         
-        return ans;
+        
+        
+        
+        
+//        int n=nums.size();
+        
+//         int ans=INT_MIN;
+        
+//         vector<int>dp(n,-1);
+        
+//         for(int i=0;i<n;i++){
+//             if(dp[i]!=-1){
+//                 ans=max(ans,dp[i]);
+//             }else{
+//                 ans=max(ans,  helper(i,nums,dp));
+//             }
+            
+//         }
+        
+//         return ans;
       
         
-    }
     
-    int helper(int i,vector<int>&nums,vector<int>&dp){
-        int ans=1;
+    
+//     int helper(int i,vector<int>&nums,vector<int>&dp){
+//         int ans=1;
         
-        if(dp[i]!=-1) return dp[i];
-        for(int j=0;j<i;j++){
-            if(nums[i]>nums[j]){
-                ans=max(ans,helper(j,nums,dp)+1);
-            }
-        }
-        return dp[i]=ans;
-    }
+//         if(dp[i]!=-1) return dp[i];
+//         for(int j=0;j<i;j++){
+//             if(nums[i]>nums[j]){
+//                 ans=max(ans,helper(j,nums,dp)+1);
+//             }
+//         }
+//         return dp[i]=ans;
+//     }
     
     
 };

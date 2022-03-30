36bcc1bcdbcc61ad3300f0bf95d342a60950733b
Ankit-op07 <85008674+Ankit-op07@users.noreply.github.com>
2022-03-30 12:15:37 +0530

    Time: 4 ms (77.73%), Space: 9.6 MB (5.69%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,55 +1,28 @@
 class Solution {
 public:
-    bool search(vector<int>v,int target){
-        int n=v.size();
-        int l=0;
-        int h=n-1;
-        while(l<=h){
-        int mid=l+(h-l)/2;
-            if(v[mid]==target){
-                return true;
-            }else if(v[mid]>target){
-                h=mid-1;
-            }else{
-                l=mid+1;
-            }
-    }
-        return false;
-    }
+   
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m= matrix.size(); //number of rows
-        int n=matrix[0].size();//number of columns
-        int i=0;
-        int j=n-1;
-        while(i<m && j>=0){
-            if(matrix[i][j]==target){
-                return true;
-            }else if(matrix[i][j]>target){
-                j--;
-            }else {
-                i++;
-            }
-        }
-        return false;
-    }
+       
+        int m=matrix.size();
+        int n=matrix[0].size();
         
+        int l=0;
+        int r=n-1;
         
-        
-        
-        
-        
-        
-        
-        
-        
-        /*
-        for(int i=0;i<m;i++){
+        while(l<m && r>=0){
+   
+          
             
-            if(search(matrix[i],target)==true){
+            if(matrix[l][r]==target){
                 return true;
+            }else if(matrix[l][r]>target){
+                r=r-1;
+            }else{
+                l=l+1;
             }
+            
         }
         return false;
     }
-    */
+     
 };

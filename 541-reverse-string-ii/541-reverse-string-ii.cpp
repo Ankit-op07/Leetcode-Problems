1e3506882911e2379587dfd8cec44871df1e7f57
Ankit-op07 <85008674+Ankit-op07@users.noreply.github.com>
2022-04-05 11:20:55 +0530

    Time: 3 ms (90.69%), Space: 7.4 MB (32.71%) - LeetHub

--- 541-reverse-string-ii/541-reverse-string-ii.cpp
+++ 541-reverse-string-ii/541-reverse-string-ii.cpp
@@ -1,11 +1,39 @@
 class Solution {
 public:
+    
+   
+         
+    
+    
+    
+  
+
     string reverseStr(string s, int k) {
        
-        for(int i=0;i<s.size();i+=2*k){
-            auto it = s.begin()+i+k > s.end() ? s.end() : s.begin()+i+k ;
+        for(int i=0;i<s.size(); i += 2*k){
+            
+            //bacdefg-
+            
+           
+            
+            auto it = (s.begin()+i+k > s.end()) ? s.end() : s.begin()+i+k;
+            
+           
+              
+            
+            
             reverse(s.begin()+i,it);
+            
+            
         }
+        
         return s;
+      
+       
+        
+        
+            
+           
+            
     }
 };

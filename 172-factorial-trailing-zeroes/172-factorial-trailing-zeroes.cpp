4333a9368de1e798e947f0e562ead5196f4ae48a
Ankit-op07 <85008674+Ankit-op07@users.noreply.github.com>
2022-04-08 01:12:47 +0530

    Time: 5 ms (24.22%), Space: 5.9 MB (75.93%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        return n == 0 ? 0 : n / 5 + trailingZeroes(n / 5);
+      
+        int ans=0;
+        
+        for(int i=1;i<=n;i++){
+            int count=0;
+            int k=i;
+           
+            while(k%10==0){
+                k/=10;
+                count++;
+            }
+            
+            
+            while(k%5==0){
+                k/=5;
+                count++;
+            }
+            
+             ans+=count;
+        }
+       return ans;
     }
 };

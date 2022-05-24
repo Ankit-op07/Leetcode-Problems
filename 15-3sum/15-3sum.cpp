4a95a805a31229995c22dfd913cd9596804ee31b
Ankit-op07 <85008674+Ankit-op07@users.noreply.github.com>
2022-05-24 20:51:50 +0530

    Time: 935 ms (11.93%), Space: 23.6 MB (25.85%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -4,20 +4,20 @@ public:
         sort(nums.begin(),nums.end());
         int n=nums.size();
         vector<vector<int>>ans;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-2;i++){
             if(i==0 || nums[i]!=nums[i-1]){
-            for(int j=i+1;j<n;j++){
+            for(int j=i+1;j<n-1;j++){
                 if(j==i+1 || nums[j]!=nums[j-1]){
                 int l = j+1;
                 int h=n-1;
                 int t=-(nums[i]+nums[j]);
-                vector<int>temp={nums[i],nums[j]};
+                vector<int>temp;
                 bool done = false;
                 while(l<=h){
                     int mid=l+(h-l)/2;
                     
                     if(nums[mid]==t){
-                        temp.push_back(nums[mid]);
+                        // temp.push_back(nums[mid]);
                         done=true;
                         break;
                     }else if(nums[mid]>t){
@@ -27,6 +27,9 @@ public:
                     }
                 }
                 if(done){
+                    temp.push_back(nums[i]);
+                    temp.push_back(nums[j]);
+                    temp.push_back(t);
                     ans.push_back(temp);
                 }
             }

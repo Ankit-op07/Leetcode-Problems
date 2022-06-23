1655c7d85df7a66685ad6e7a08554cf3034e6a85
Ankit-op07 <85008674+Ankit-op07@users.noreply.github.com>
2022-06-23 23:30:23 +0530

    Time: 232 ms (52.16%), Space: 67.6 MB (76.78%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,34 +1,23 @@
-class Solution {
-public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
-     vector<int>index;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]%2!=0){
-            index.push_back(i);
+class Solution
+{
+    public:
+        int numberOfSubarrays(vector<int> &nums, int k)
+        {
+            int j = 0, odd = 0, count = 0, total = 0;
+            for (int i = 0; i < nums.size(); i++)
+            {
+                if (nums[i] &1) // for odds
+                {
+                    odd++;
+                    if (odd >= k)
+                    {
+                        count = 1;
+                        while (!(nums[j++] &1)) count++;
+                        total += count;
+                    }
+                }
+                else if (odd >= k) total += count;
             }
+            return total;
         }
-        int ans=0;
-        for(int i=k-1;i<index.size();i++){
-            int a;
-            if(i!=index.size()-1){
-                a=index[i+1]-index[i];
-            }else{
-                a=nums.size()-index[i];
-            }
-            
-            int j=i-k+1;
-            
-            int b;
-            if(j==0){
-                b=index[j]+1;
-            }else{
-                b=index[j]-index[j-1];
-                 // cout<<nums[j]<<" "<<nums[j-1]<<endl;
-            }
-            // cout<<a<<" "<<b<<endl;
-            ans += a*b;
-            
-        }
-        return ans;
-    }
 };

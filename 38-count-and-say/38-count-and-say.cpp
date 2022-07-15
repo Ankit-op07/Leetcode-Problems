0b8d0f8ecf8b62244bff4ff947c73eb69b217c7f
Ankit-op07 <85008674+Ankit-op07@users.noreply.github.com>
2022-07-15 17:12:24 +0530

    Time: 20 ms (31.90%), Space: 7.2 MB (27.23%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -1,55 +1,69 @@
-class Solution {
-public:
-    string fun(string s,int n){
-        if(n==4){
-            return s;
+class Solution
+{
+    public:
+
+        string fun(string s, int n)
+        {
+
+            if (n == 4)
+            {
+                return s;
+            }
+
+            string ans;
+
+            int cnt = 1;
+           	//233215
+            int i = 1;
+           	//1
+           	//cnt number
+            while (i < s.size())
+            {
+                if (s[i] == s[i - 1])
+                {
+                    cnt++;
+                }
+                else
+                {
+                    stringstream ss;
+                    ss << cnt;
+                    char c;
+                    ss >> c;
+
+                    ans.push_back(c);
+                    ans.push_back(s[i - 1]);
+                    cnt = 1;
+                }
+                i++;
+
+                if (i == s.size())
+                {
+                    stringstream ss;
+                    ss << cnt;
+                    char c;
+                    ss >> c;
+
+                    ans.push_back(c);
+                    ans.push_back(s[i - 1]);
+                }
+            }
+           return fun(ans,n-1);
         }
-           string ans;
-        int i=1;
-        int count=1; //1112
-    while(i<s.size()){
-        if(s[i]==s[i-1]){
-            count++;
-        }else{
-           
-            stringstream ss;
-            ss<<count;
-            char c;
-            ss>>c;
-            
-            ans.push_back(c);
-            ans.push_back(s[i-1]);
-             count=1;
-        }
-        
-        i++;
-        if(i==s.size()){
-             stringstream ss;
-            ss<<count;
-            char c;
-            ss>>c;
-            ans.push_back(c);
-            ans.push_back(s[i-1]);
-        }
-    }
-        return fun(ans,n-1);
-        
-    }
-    
-    string countAndSay(int n) {
-        
-        if(n==1){
+    string countAndSay(int n)
+    {
+        if (n == 1)
+        {
             return "1";
-        }else if(n==2){
-            return "11";
-        }else if(n==3){
-            return "21"; //count-value,count-value
         }
-        
-        return fun("1211",n);
-       
-       
-       
-        
+        else if (n == 2)
+        {
+            return "11";
+        }
+        else if (n == 3)
+        {
+            return "21";
+        }
+
+        return fun("1211", n);
     }
 };

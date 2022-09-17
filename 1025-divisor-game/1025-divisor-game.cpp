09ad0e47fc4d3ae02488548e65a011b93251a816
Ankit-op07 <85008674+Ankit-op07@users.noreply.github.com>
2022-09-17 11:24:42 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (32.03%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,37 +1,58 @@
 class Solution {
 public:
-    int dp[1001];
+//     int dp[1001];
     
-     int fun(int n){
+//      int fun(int n){
          
-         if(n == 1){
-            return dp[1] = 0;
-         }
+//          if(n == 1){
+//             return dp[1] = 0;
+//          }
          
-         for(int i=1;i<n;i++){
+//          for(int i=1;i<n;i++){
              
-             if(n%i==0){
+//              if(n%i==0){
                  
-                 if(dp[n-i]!=-1 && dp[n-1]==0){
-                     return dp[n]=1;
-                 }
+//                  if(dp[n-i]!=-1 && dp[n-1]==0){
+//                      return dp[n]=1;
+//                  }
                  
                  
-                 if(fun(n-i)==0){
-                     return dp[n]=1;
+//                  if(fun(n-i)==0){
+//                      return dp[n]=1;
                      
-                 }
-             }
-         }
-         return dp[n]=0;
+//                  }
+//              }
+//          }
+//          return dp[n]=0;
          
          
         
-    }
+//     }
     
     bool divisorGame(int n) {
+       //  memset(dp,-1,sizeof dp);
+       // return fun(n);
+        
+        int dp[1001];
         memset(dp,-1,sizeof dp);
-       return fun(n);
+        
+        dp[1]=0;
+        
+        for(int i=2;i<=n;i++){
+            bool flag = false;
+            for(int j=1;j<i;j++){
+                if(i%j==0){
+                    if(dp[i-j]==0){
+                        dp[i]=1;
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+            if(!flag) dp[i]=0;
+        }
+        
+        return dp[n];        
 
     }
     
